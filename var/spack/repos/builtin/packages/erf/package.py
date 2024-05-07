5565c88c20b689bd028a2373739939fb9e738353
Jean M. Sexton <jmsexton@lbl.gov>
2024-05-07 14:03:52 -0700

    Test with submodule included release

--- var/spack/repos/builtin/packages/erf/package.py
+++ var/spack/repos/builtin/packages/erf/package.py
@@ -43,14 +43,14 @@ class Erf(CMakePackage, CudaPackage, ROCmPackage):
  Arakawa C-grid for large-scale weather modeling."""
 
     homepage = "https://erf.readthedocs.io/en/latest/index.html"
-    url      = "https://github.com/erf-model/ERF/archive/refs/tags/24.03.tar.gz"
+    url      = "https://github.com/jmsexton03/ERF/releases/download/24.05/ERF-24.05.tar.gz"
     git      = "https://github.com/erf-model/ERF.git"
    
     maintainers = ['WeiqunZhang', 'asalmgren', 'jmsexton03', 'AMLattanzi']
 
     version('develop', branch='development', submodules=True)
 #    version('develop', branch='development')
-    version('24.03', sha256='351dca62d3dddfff613010f5fcbfc3a915e9146606716b4c8326ebfafdd05f10')
+#    version('24.03', sha256='351dca62d3dddfff613010f5fcbfc3a915e9146606716b4c8326ebfafdd05f10')
 
     # Config options
     variant('dimensions', default='3',
